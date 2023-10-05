9d0500e2ce2fe6021392d7764787637fd0f5a243
Somaditya Basak <mail@somaditya.com>
2023-10-05 13:30:27 +0530

    mvp

---
+++ src/components/App.tsx
@@ -0,0 +1,11 @@
+import { Timer } from "./Timer";
+import "../css/App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Visual Timer</h1>
+      <Timer />
+    </div>
+  );
+}
